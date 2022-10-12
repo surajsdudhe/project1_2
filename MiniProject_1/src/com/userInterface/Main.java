dca6da0e2f4a537ff8758188d572fa80c7ee0c44
netware <netware@DESKTOP-E9M0GDP>
2022-10-12 23:39:00 +0530

    2nd change

--- MiniProject_1/src/com/userInterface/Main.java
+++ MiniProject_1/src/com/userInterface/Main.java
@@ -3,7 +3,8 @@ package com.userInterface;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello git");//surajsatyawandudhe
+		System.out.println("hello git");//surajsatyawandudhe is password
+		System.out.println("hello git");//surajsatyawandudhe is password
 
 	}
 
